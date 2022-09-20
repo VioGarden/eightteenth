f7c1da45a77c42ef7117f7f13d50994c4b068c5c
VioGarden <21AK191@queensu.ca>
2022-09-19 21:38:57 -0400

    registerform

--- mysongusers/forms.py
+++ mysongusers/forms.py
@@ -3,13 +3,22 @@ from django.contrib.auth.models import User
 from django import forms
 
 class RegisterUserForm(UserCreationForm):
-    email = forms.EmailField(required=False)
+    email = forms.EmailField(
+        required=False, 
+        widget=forms.EmailInput(
+        attrs={'class':'register-form', 'placeholder': 'email (optional)'}))
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
     def __init__(self, *args, **kwargs):
         super(RegisterUserForm, self).__init__(*args, **kwargs)
 
-        self.fields['username']
-        self.fields['password1']
-        self.fields['password2']
+        self.fields['username'].widget.attrs['class'] = 'register-form'
+        self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['username'].help_text = 'Letters, digits and @/./+/-/_ only'
+        self.fields['password1'].widget.attrs['class'] = 'register-form'
+        self.fields['password1'].widget.attrs['placeholder'] = 'password'
+        # self.fields['password1'].help_text = ''
+        self.fields['password2'].widget.attrs['class'] = 'register-form'
+        self.fields['password2'].widget.attrs['placeholder'] = 'password (confirm)'
+        # self.fields['password2'].help_text = ''
